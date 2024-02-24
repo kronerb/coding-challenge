c2eded7787adeeffb63456b078fa1ada7152df18
benjaminkroner <benjamin.kroner@stud.th-deg.de>
2024-02-24 10:09:48 +0100

    initial commit

---
+++ src/main/java/com/codingchallenge/projectcodingchallenge/ProjectCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.projectcodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingchallenge/projectcodingchallenge/ProjectCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.projectcodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
